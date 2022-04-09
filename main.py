f9872db9c22c07646bfdf7360488933d9885cc5d
Lipakumu <bonheur.christian@gmail.com>
2022-04-09 10:48:13 +0100

    simple envoie de message sur whatsapp avec python

---
+++ main.py
@@ -0,0 +1,5 @@
+import pywhatkit as whatsapp
+
+#dans la parentese metre en 1er lieu le numero suivi du message a envoyer
+
+whatsapp.sendwhatmsg('+24162131895', "#MasterLipakumu : hi i love MasterLipakumu and you ?", 23, 5)
